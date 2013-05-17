743bdea72d44c66ad09ceca53dd7ca2ea2347e83
Gervel Giva <gervel@caresharing.ph>
2013-05-17 16:17:10 +0800

    Added flash messages

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express')
   , page = require('./routes/page')
   , http = require('http')
   , path = require('path')
-  , mongoose = require('mongoose');
+  , flash = require('connect-flash');
+//  , mongoose = require('mongoose')
+//  , Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost/nlms');
+//mongoose.connect('mongodb://localhost/nlms');
 
 var app = express();
 
@@ -18,6 +20,7 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
+app.use(flash());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,6 +31,17 @@ if ('development' == app.get('env')) {
 
 app.get('/', page.index);
 app.get('/login', page.login);
+app.post('/login_process', page.login_process);
+
+
+/*//model schema for mongo
+var Staff = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true, default: "ojt" },
+  created_at: { type: Date, default: Date.now }
+});
+*/
 
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/page.js
+++ routes/page.js
@@ -3,16 +3,29 @@ exports.index = function(req, res){
     data = { title: 'NCR Laboratory Management System' };
     res.render('index', data);
   } else{
-    req.session.authrequired = true;
+    req.flash('authRequired', true)
     res.redirect("/login");
   }
 };
 
 exports.login = function(req, res){
   data = { title: 'NCR Laboratory Management System | Login' };
-  data.authrequired = req.session.authrequired;
-  req.session.authrequired = false;
+  data.authrequired = req.flash("authRequired")[0];
 
   res.render('login', data);
+};
+
+
+exports.login_process = function(req, res){
+  temp_cred = {username : "carlo", password: "1234qwer" }; // test user
+
+  /* todo: check if user is in the database, if so, then save the username session and redirect to homepage. */
+  if(req.body.username == temp_cred.username && req.body.password == temp_cred.password){
+    console.log("success");
+  }else{
+    console.log("failed");
+  }
+
+  res.redirect('/login');
 };
 
